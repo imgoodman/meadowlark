575b18dcc8e08e3860b80fc8bd26d0d256bce1ed
jian <jan8611@163.com>
2016-04-14 19:18:11 +0800

    chapter-3

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 var express=require('express');
 
 var app=express();
+app.use(express.static(__dirname+'/public'));
+
+//设置handlebars视图引擎
+var handlebars=require('express-handlebars').create({
+	defaultLayout:'main'//默认布局
+});
+//设置express的视图引擎
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+
+//home
 app.get('/',function(req,res){
-	res.type('text/plain');
-	res.send('Meadowlark works');
+	res.render('home.handlebars');
 });
 
+//about
+app.get('/about',function(req,res){
+	res.render('about');
+});
 //400
 app.use(function(req,res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 -- Not found');
+	res.render('404');
 });
 
 //500
 app.use(function(err,req,res,next){
 	console.log(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 -- Internal server error');
+	res.render('500');
 });
 
 app.listen(app.get('port'),function(){
