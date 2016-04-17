61d722e15df49b5c39ead3be1573f1265d21a160
jian <jan8611@163.com>
2016-04-17 13:47:38 +0800

    chapter 8

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var express=require('express');
 
 var app=express();
 app.use(express.static(__dirname+'/public'));
+app.use(require('body-parser')());
+app.disable('x-powered-by');
 
 //设置handlebars视图引擎
 var handlebars=require('express-handlebars').create({
-	defaultLayout:'main'//默认布局
+	defaultLayout:'main',//默认布局
+	helpers:{
+		section:function(name,options){
+			if(!this._sections) this._sections={};
+			this._sections[name]=options.fn(this);
+			return null;
+		}
+	}
 });
 //设置express的视图引擎
 app.engine('handlebars',handlebars.engine);
@@ -23,13 +32,35 @@ app.use(function(req,res,next){
 
 //home
 app.get('/',function(req,res){
-	res.render('home.handlebars');
+	res.render('home');
 });
 
 //about
 app.get('/about',function(req,res){
 	res.render('about');
 });
+
+//newsletter
+app.get('/newsletter',function(req,res){
+	res.render('newsletter',{
+		csrf:'CSRF token here'
+	})
+});
+app.post('/process',function(req,res){
+	console.log('Form (from querystring): '+req.query.form);
+	console.log('CSRF token (from hidden form field):'+req.body._csrf);
+	console.log('Name (from visible form field): '+req.body.name);
+	console.log('Email (from visible form field): '+req.body.email);
+	if(req.xhr || req.accepts('json,html')==='json'){
+		res.json({success:true})
+	}else{
+	res.redirect(303,'/thank-you');
+	}
+});
+app.get('/thank-you',function(req,res){
+	res.render('thank-you');
+});
+
 //400
 app.use(function(req,res){
 	res.status(404);
